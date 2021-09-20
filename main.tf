8feb3453e447381424c827ebc457320dd705116f
Rik Lempens <rik@lempens.net>
2021-09-20 16:19:38 +0200

    Removed silenced as is it depricated in favor of downtimes

--- main.tf
+++ main.tf
@@ -86,11 +86,3 @@ resource "datadog_monitor" "generic_datadog_monitor" {
   }
 
   locked = var.locked
-
-  # We don't want to manage muted alerts in Terraform.
-  lifecycle {
-    ignore_changes = [
-      silenced
-    ]
-  }
-}
